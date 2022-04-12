c88118cdcf6d3619a5c20989b6ef2aa2943e6e0c
Reballi <srikanth.raj18@gmail.com>
2022-04-12 12:59:05 +0530

    initial commit

---
+++ program/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+        int num1 = 10;
+        int num2 = 20;
+        Swap(ref num1, ref num2);
+        Console.WriteLine("num 1:{0}, and num 2 :{2}",num1,num2);
+          }
+    static void Swap(ref int x, ref int y) {
+        int temp = x;
+        x = y;
+            y = temp;
+    }
+}
